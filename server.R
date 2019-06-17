99eafbce094bca9046ad819f0125915af3ec5e39
bmetenko <bohdanmetenko@outlook.com>
2019-06-16 23:58:26 -0400

    Category Pie Implementation and Modifications

--- server.R
+++ server.R
@@ -1,20 +1,62 @@
 library(shiny)
 library(shinydashboard)
+library(tidyverse)
+library(cowplot)
+# library(plotly)
 
 # TODO: try catch on data load. CSV output.
 # devtools::install_github("ramamet/applestoreR")
 df <- applestoreR::AppleStore
 
+
+
+blank_theme <- theme_minimal()+
+  theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    panel.border = element_blank(),
+    panel.grid=element_blank(),
+    axis.ticks = element_blank(),
+    plot.title=element_text(size=14, face="bold")
+  )
+
 server <- function(input, output, session) {
-  output$plot1 <- renderPlot({
-    priceTally <- df %>% group_by(input$cat) %>% tally() 
-    
-    pie(x = priceTally[,2], labels = priceTally[,1])
-  })
+  
   
   output$input1 <- renderUI({
-    ch <- df[, sapply(df, class) == "numeric"] %>% colnames()
-    selectInput(inputId = "cat", choices = ch, label = "Category")
+    checkboxInput(inputId = "naOmit", label = "Omit NAs?", value = FALSE)
+  })
+  
+  output$topCat<- renderUI({
+    sliderInput(inputId = "catNum", 
+                label = "Number of top categories?", min = 1, max = 23, value = 23, step = 1)
+  })
+  
+  
+  output$plot1 <- renderPlot({
+    CatTally <- df %>% group_by(prime_genre) %>% tally()
+    
+    if (input$naOmit == TRUE) {
+      j <- dim(CatTally)[1]
+      CatTally <- CatTally[-((input$catNum):24),]
+      CatTally <- CatTally[order(CatTally$n, decreasing = T),]
+      CatTally$prime_genre <- factor(x = CatTally$prime_genre, levels = CatTally$prime_genre)
+      
+    } else {
+      CatTally <- CatTally[c(1:input$catNum, 24),]
+      CatTally <- CatTally[order(CatTally$n, decreasing = T),]
+      CatTally$prime_genre <- factor(x = CatTally$prime_genre,levels = CatTally$prime_genre)
+    }
+    
+    print(CatTally$prime_genre)
+    
+    bp<- ggplot(CatTally, aes(x="", y=n, 
+                              fill=as.factor(prime_genre)))+
+      geom_bar(width = 1, color ="white", stat = "identity") + 
+      coord_polar("y", start=0) + blank_theme +
+      scale_fill_discrete(name = "App Genre")
+    
+    bp
   })
 }
 
--- ui.R
+++ ui.R
@@ -2,19 +2,30 @@ library(shiny)
 library(shinydashboard)
 
 
-ui <- dashboardPage(skin = "black",
+ui <- dashboardPage(
+  skin = "black",
   dashboardHeader(title = "App Store Data (2017)"),
   dashboardSidebar(sidebarMenu(
     menuItem(text = "Data Summary", tabName = "Summary"),
     menuItem(text = "Data Table", tabName = "Table"),
     menuItem(text = "Data Plots", tabName = "Plots")
-  )
-  ),
-  dashboardBody(
-    fluidPage(
-      box(plotOutput("plot1"),
-          box(uiOutput(outputId = "input1"))
-      
+  )),
+  dashboardBody(fluidPage(
+    box(
+      plotOutput("plot1"),
+      width = "100%",
+      status = "success",
+      solidHeader = TRUE,
+      align = "center",
+      title = "App Category Distribution",
+      box(
+        uiOutput(outputId = "input1"),
+        solidHeader = T,
+        title = "Clean?",
+        status = "warning"
+      ),
+      box(uiOutput(outputId = "topCat"))
     )
-  )
-))
+    
+  ))
+)
