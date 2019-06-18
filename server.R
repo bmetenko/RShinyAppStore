a0fb400f23e266d39f741b548f2c053d5cebe890
bmetenko <bohdanmetenko@outlook.com>
2019-06-17 20:59:44 -0400

    Added data CSV, updated ui, tabs, and reactivity.

--- server.R
+++ server.R
@@ -5,58 +5,108 @@ library(cowplot)
 # library(plotly)
 
 # TODO: try catch on data load. CSV output.
+
+
 # devtools::install_github("ramamet/applestoreR")
-df <- applestoreR::AppleStore
+# df <- applestoreR::AppleStore
+# write.csv(df, file = "appleData.csv")
 
+df <- read.csv(file = "appleData.csv")
 
-
-blank_theme <- theme_minimal()+
+blank_theme <- theme_minimal() +
   theme(
     axis.title.x = element_blank(),
     axis.title.y = element_blank(),
     panel.border = element_blank(),
-    panel.grid=element_blank(),
+    panel.grid = element_blank(),
     axis.ticks = element_blank(),
-    plot.title=element_text(size=14, face="bold")
+    plot.title = element_text(size = 14, face = "bold")
   )
 
 server <- function(input, output, session) {
-  
-  
-  output$input1 <- renderUI({
-    checkboxInput(inputId = "naOmit", label = "Omit NAs?", value = FALSE)
-  })
-  
-  output$topCat<- renderUI({
-    sliderInput(inputId = "catNum", 
-                label = "Number of top categories?", min = 1, max = 23, value = 23, step = 1)
-  })
-  
-  
-  output$plot1 <- renderPlot({
+  dataParse <- reactive({
+    ## Start Reactive wrap
     CatTally <- df %>% group_by(prime_genre) %>% tally()
     
     if (input$naOmit == TRUE) {
       j <- dim(CatTally)[1]
-      CatTally <- CatTally[-((input$catNum):24),]
-      CatTally <- CatTally[order(CatTally$n, decreasing = T),]
-      CatTally$prime_genre <- factor(x = CatTally$prime_genre, levels = CatTally$prime_genre)
+      CatTally <- CatTally[-((input$catNum):24), ]
+      CatTally <- CatTally[order(CatTally$n, decreasing = T), ]
+      CatTally$prime_genre <-
+        factor(x = CatTally$prime_genre,
+               levels = CatTally$prime_genre)
       
     } else {
-      CatTally <- CatTally[c(1:input$catNum, 24),]
-      CatTally <- CatTally[order(CatTally$n, decreasing = T),]
-      CatTally$prime_genre <- factor(x = CatTally$prime_genre,levels = CatTally$prime_genre)
+      CatTally <- CatTally[c(1:input$catNum, 24), ]
+      CatTally <- CatTally[order(CatTally$n, decreasing = T), ]
+      CatTally$prime_genre <-
+        factor(x = CatTally$prime_genre,
+               levels = CatTally$prime_genre)
     }
     
-    print(CatTally$prime_genre)
+    CatTally
+  })
+  
+  output$input1 <- renderUI({
+    checkboxInput(inputId = "naOmit",
+                  label = "Omit NAs?",
+                  value = FALSE)
+  })
+  
+  output$topCat <- renderUI({
+    sliderInput(
+      inputId = "catNum",
+      label = "Number of top categories?",
+      min = 1,
+      max = 23,
+      value = 23,
+      step = 1
+    )
+  })
+  
+  
+  output$plot1 <- renderPlot({
+    CatTally <- dataParse()
     
-    bp<- ggplot(CatTally, aes(x="", y=n, 
-                              fill=as.factor(prime_genre)))+
-      geom_bar(width = 1, color ="white", stat = "identity") + 
-      coord_polar("y", start=0) + blank_theme +
+    bp <- ggplot(CatTally, aes(
+      x = "",
+      y = n,
+      fill = as.factor(prime_genre)
+    )) +
+      geom_bar(width = 1,
+               color = "white",
+               stat = "identity") +
+      blank_theme +
       scale_fill_discrete(name = "App Genre")
+    # assign(x = "bp", value = bp, envir = .GlobalEnv)
+    bp + coord_polar("y", start = 0)
+  })
+  
+  output$plot2 <- renderPlot({
+    dataParse()
     
+    CatTally <- dataParse()
+    
+    bp <- ggplot(CatTally,
+                 aes(
+                   x = CatTally$prime_genre,
+                   y = CatTally$n,
+                   fill = prime_genre
+                 )) +
+      geom_bar(stat = "identity") +
+      blank_theme + theme(legend.position = "none",
+                          axis.text.x = element_text(angle = 45))
     bp
+    
+  })
+  
+  output$table1 <- renderTable({
+    g <- dataParse()
+    
+    g
+  })
+  
+  output$table2 <- renderTable({
+    head(df, 5)
   })
 }
-
--- ui.R
+++ ui.R
@@ -4,28 +4,76 @@ library(shinydashboard)
 
 ui <- dashboardPage(
   skin = "black",
-  dashboardHeader(title = "App Store Data (2017)"),
-  dashboardSidebar(sidebarMenu(
-    menuItem(text = "Data Summary", tabName = "Summary"),
-    menuItem(text = "Data Table", tabName = "Table"),
-    menuItem(text = "Data Plots", tabName = "Plots")
-  )),
-  dashboardBody(fluidPage(
-    box(
-      plotOutput("plot1"),
-      width = "100%",
-      status = "success",
-      solidHeader = TRUE,
-      align = "center",
-      title = "App Category Distribution",
-      box(
-        uiOutput(outputId = "input1"),
-        solidHeader = T,
-        title = "Clean?",
-        status = "warning"
-      ),
-      box(uiOutput(outputId = "topCat"))
+  dashboardHeader(
+    title = "App Store Data (2017)",
+    dropdownMenu(
+      type = "messages",
+      messageItem(
+        from = "Author",
+        message = "Welcome. Enjoy your stay.",
+        icon = icon("tags"), href = "https://github.com/bmetenko",
+        time = "2019-06-17 8:50pm"
+      )
+    ),
+    dropdownMenu(
+      type = "tasks",
+      badgeStatus = "success",
+      taskItem(value = 90, color = "green",
+               "Git Re-commit"),
+      taskItem(value = 75, color = "blue",
+               "Server deployment")
     )
+  ),
+  dashboardSidebar(sidebarMenu(
+    menuItem(
+      text = tagList(icon("dashboard", lib = "glyphicon"), "Data Plots"),
+      tabName = "Plots",
+      icon =
+    ),
+    menuItem(text = tagList(
+      icon("list-alt", lib = "glyphicon"), "Data Table"
+    ), tabName = "Table"),
+    menuItem(text = tagList(
+      icon("align-left", lib = "glyphicon"), "Data Summary"
+    ), tabName = "Summary")
     
-  ))
+    
+  )),
+  dashboardBody(fluidPage(tabItems(
+    tabItem(
+      tabName = "Plots",
+      box(
+        plotOutput("plot1"),
+        width = "100%",
+        status = "success",
+        solidHeader = TRUE,
+        align = "center",
+        title = tagList(icon("cog",
+                             lib = "glyphicon"),
+                        "App Category Distribution"),
+        box(
+          uiOutput(outputId = "input1"),
+          solidHeader = T,
+          title = "Clean?",
+          status = "warning"
+        ),
+        box(uiOutput(outputId = "topCat"), solidHeader = TRUE)
+      ),
+      box(
+        title = tagList(icon("signal",
+                             lib = "glyphicon"), "Histogram Distribution"),
+        plotOutput("plot2"),
+        solidHeader = T,
+        width = "100%",
+        status = "primary"
+      )
+      
+      
+    ),
+    tabItem(tabName = "Summary", h2("Hello")),
+    tabItem(tabName = "Table", h2("Hello"), fluidPage(# box(tableOutput("table1"), width = "20%"),
+      box(
+        tableOutput("table2"), width = "80%"
+      )))
+  )))
 )
