a56be2bc91793feb4a2300cee819ef63324c4a50
bmetenko <bohdanmetenko@outlook.com>
2019-07-30 22:40:22 -0400

    Updated package check for easy dev environment configuration on new machines.

--- package_check.R
+++ package_check.R
@@ -1,7 +1,14 @@
-required_pkg = c("shiny", "ggplot2", "dplyr","jpeg", "shinydashboard", "httr", "jsonlite", "scales", "cowplot", "tidyverse")
+required_pkg = c("shiny", "ggplot2", 
+                 "dplyr","jpeg", 
+                 "shinydashboard", "httr", 
+                 "forcats", "jsonlite", 
+                 "sqldf", "RColorBrewer", 
+                 "ggpubr",
+                 "scales", "cowplot",
+                 "tidyverse")
 
 packages_to_add = required_pkg[!(required_pkg %in% installed.packages()[, 1])]
 
 if (length(packages_to_add) > 0) {
-  install.packages(packages_to_add, repos = "https://cran.rstudio.com")
+  install.packages(packages_to_add,dependencies = T, repos = "https://cran.rstudio.com")
 }
